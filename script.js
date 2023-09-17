0cf8e8f8f72e27309b0d08e490ca34877b016ea5
penass <45908692+penass@users.noreply.github.com>
2023-09-16 18:42:26 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,70 @@
+const imageSelector = document.getElementById("imageSelector");
+const displayedImage = document.getElementById("displayedImage");
+
+// Load JSON data
+fetch("data.json")
+    .then(response => response.json())
+    .then(data => {
+        // Build option list and category filter
+        const optionList = document.createElement("select");
+        const categoryFilter = new Set();
+
+        data.forEach(item => {
+            const option = document.createElement("option");
+            option.value = item.option;
+            option.textContent = item.option;
+            optionList.appendChild(option);
+            categoryFilter.add(item.category);
+        });
+
+        imageSelector.appendChild(optionList);
+
+        // Build category filter options
+        const categorySelector = document.createElement("select");
+        const defaultCategoryOption = document.createElement("option");
+        defaultCategoryOption.value = "";
+        defaultCategoryOption.textContent = "All Categories";
+        categorySelector.appendChild(defaultCategoryOption);
+
+        categoryFilter.forEach(category => {
+            const option = document.createElement("option");
+            option.value = category;
+            option.textContent = category;
+            categorySelector.appendChild(option);
+        });
+
+        // Attach event listener to category selector
+        categorySelector.addEventListener("change", () => {
+            const selectedCategory = categorySelector.value;
+            optionList.innerHTML = ""; // Clear existing options
+
+            data.forEach(item => {
+                if (selectedCategory === "" || item.category === selectedCategory) {
+                    const option = document.createElement("option");
+                    option.value = item.option;
+                    option.textContent = item.option;
+                    optionList.appendChild(option);
+                }
+            });
+
+            updateDisplayedImage();
+        });
+
+        // Append category selector to the form
+        const form = document.getElementById("imageForm");
+        form.insertBefore(categorySelector, optionList);
+
+        // Function to update displayed image
+        function updateDisplayedImage() {
+            const selectedOption = imageSelector.value;
+            const selectedData = data.find(item => item.option === selectedOption);
+            displayedImage.src = selectedData.imageUrl;
+        }
+
+        // Attach event listener to option selector
+        optionList.addEventListener("change", updateDisplayedImage);
+
+        // Initialize the displayed image on page load
+        updateDisplayedImage();
+    })
+    .catch(error => console.error("Error loading data:", error));
